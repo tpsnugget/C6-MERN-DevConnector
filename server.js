164eba68564a79319a64080d6f71de9d4d1be65b
Michael Giebner <mikegiebner@gmail.com>
2019-11-23 10:53:29 -0500

    C6-2-8 complete

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app = express()
 
 connectDB()
 
+// Connect Database
 app.get("/", (req, res) => res.send("API Running"))
+
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/users", require("./routes/api/users"))
 
 const PORT = process.env.PORT || 5000
 
